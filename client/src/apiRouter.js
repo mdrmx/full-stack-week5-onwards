571d871ce90106003a71d5b16d1c5e5c074408c8
Stuart Haffenden <stuart.mdrmx@gmail.com>
2026-03-05 22:12:15 +0000

    final commit

--- client/src/apiRouter.js
+++ client/src/apiRouter.js
@@ -3,13 +3,10 @@ import { dailyForecast } from "./components/ui_components/weatherTile.js";
 // call the local express API rather than hitting OpenWeather directly
 export async function fetchWeather({ placename }) {
   let url;
-
   // build query string based on what was provided
-  const params = new URLSearchParams();
   if (placename) url = `/weather?placename=${placename}`;
 
-  // make request to our own server
-
+  // make request to our server
   try {
     const response = await fetch(url);
     const data = await response.json();
